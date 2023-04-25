8f00dc6f920cac7b43c38e1ccfdbe93c03725dbe
Omer Hamerman <omerxx@gmail.com>
2023-04-25 13:09:19 +0100

    cleaner code

--- main.go
+++ main.go
@@ -9,22 +9,22 @@ import (
 )
 
 func main() {
-	err := initDB()
-	if err != nil {
+	var bot *tgbotapi.BotAPI
+	var err error
+	var updates tgbotapi.UpdatesChannel
+	if err := initDB(); err != nil {
 		log.Fatal(err)
 	}
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
-	if err != nil {
-		log.Fatal(err.Error())
+	if bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN")); err != nil {
+		log.Fatal(err)
+	} else {
+		go tick(bot, time.NewTicker(24*time.Hour), make(chan bool))
+		bot.Debug = false
+		log.Infof("Authorized on account %s", bot.Self.UserName)
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+		updates = bot.GetUpdatesChan(u)
 	}
-	go tick(bot, time.NewTicker(24*time.Hour), make(chan bool))
-
-	bot.Debug = false
-	log.Infof("Authorized on account %s", bot.Self.UserName)
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message updates
