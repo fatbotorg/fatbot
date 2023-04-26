21f96e6ba6ce79ceabeee5c46f0b059962a96817
Omer Hamerman <omerxx@gmail.com>
2023-04-26 23:02:51 +0100

    no more missing photo

--- handlers.go
+++ handlers.go
@@ -12,17 +12,21 @@ func handleStatusCommand(update tgbotapi.Update) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 	user := getUser(update.Message)
 
-	if user.LastWorkout.IsZero() {
+	lastWorkout, err := getLastWorkout(user.TelegramUserID)
+	if err != nil {
+		log.Errorf("Err getting last workout: %s", err)
+		return msg
+	}
+	if lastWorkout.CreatedAt.IsZero() {
 		log.Warn("no last workout")
 		msg.Text = "I don't have your last workout yet."
 	} else {
-		lastworkout := user.LastWorkout
 		currentTime := time.Now()
-		diff := currentTime.Sub(lastworkout)
+		diff := currentTime.Sub(lastWorkout.CreatedAt)
 		days := int(5 - diff.Hours()/24)
 		msg.Text = fmt.Sprintf("%s, your last workout was on %s\nYou have %d days and %d hours left.",
 			user.Name,
-			user.LastWorkout.Weekday(),
+			lastWorkout.CreatedAt.Weekday(),
 			days,
 			120-int(diff.Hours())-24*days-1,
 		)
@@ -34,14 +38,20 @@ func handleShowUsersCommand(update tgbotapi.Update) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 	users := getUsers()
 	message := ""
+	var lastWorkoutStr string
 	for _, user := range users {
-		var lastWorkout string
-		if user.LastWorkout.IsZero() {
-			lastWorkout = "no record"
-		} else {
-			lastWorkout = user.LastWorkout.Weekday().String()
+		lastWorkout, err := getLastWorkout(user.TelegramUserID)
+		if err != nil {
+			log.Errorf("Err getting last workout: %s", err)
+			return msg
 		}
-		message = message + fmt.Sprintf("%s [%s]", user.Name, lastWorkout) + "\n"
+		if lastWorkout.CreatedAt.IsZero() {
+			lastWorkoutStr = "no record"
+		} else {
+			hour, min, _ := lastWorkout.CreatedAt.Clock()
+			lastWorkoutStr = fmt.Sprintf("%s, %d:%d", lastWorkout.CreatedAt.Weekday().String(), hour, min)
+		}
+		message = message + fmt.Sprintf("%s [%s]", user.Name, lastWorkoutStr) + "\n"
 	}
 	msg.Text = message
 	return msg
@@ -49,22 +59,20 @@ func handleShowUsersCommand(update tgbotapi.Update) tgbotapi.MessageConfig {
 
 func handleWorkoutCommand(update tgbotapi.Update, bot *tgbotapi.BotAPI) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
-	recordUser := getUser(update.Message)
-	var message string
-	messageAddition := ""
-	if recordUser.PhotoUpdate.IsZero() || time.Now().Sub(recordUser.PhotoUpdate).Hours() > 24 {
-		messageAddition = "\nDon't forget to send a photo!"
-		handleMissingPhoto(bot, recordUser)
+	user := getUser(update.Message)
+	lastWorkout, err := getLastWorkout(user.TelegramUserID)
+	if err != nil {
+		log.Errorf("Err getting last workout: %s", err)
+		return msg
 	}
-	updateWorkout(update.Message.From.ID, 0)
-	if recordUser.LastWorkout.IsZero() {
-		message = fmt.Sprintf("%s nice work!\nThis is your first workout%s",
+	var message string
+	updateWorkout(update.Message.From.ID, update.Message.MessageID)
+	if lastWorkout.CreatedAt.IsZero() {
+		message = fmt.Sprintf("%s nice work!\nThis is your first workout",
 			update.Message.From.FirstName,
-			messageAddition,
 		)
 	} else {
-		hours := time.Now().Sub(recordUser.LastWorkout).Hours()
+		hours := time.Now().Sub(lastWorkout.CreatedAt).Hours()
 		timeAgo := ""
 		if int(hours/24) == 0 {
 			timeAgo = fmt.Sprintf("%d hours ago", int(hours))
@@ -72,49 +80,14 @@ func handleWorkoutCommand(update tgbotapi.Update, bot *tgbotapi.BotAPI) tgbotapi
 			days := int(hours / 24)
 			timeAgo = fmt.Sprintf("%d days and %d hours ago", days, int(hours)-days*24)
 		}
-		message = fmt.Sprintf("%s nice work!\nYour last workout was on %s (%s)%s",
+		message = fmt.Sprintf("%s nice work!\nYour last workout was on %s (%s)",
 			update.Message.From.FirstName,
-			recordUser.LastWorkout.Weekday(),
+			lastWorkout.CreatedAt.Weekday(),
 			timeAgo,
-			messageAddition,
 		)
 	}
+
 	msg.Text = message
+	msg.ReplyToMessageID = update.Message.MessageID
 	return msg
 }
-
-func handleMissingPhoto(bot *tgbotapi.BotAPI, user User) {
-	msg := tgbotapi.NewMessage(user.TelegramUserID, "Please upload a photo to the group in the next 10 minutes")
-	bot.Send(msg)
-}
-
-// TODO:
-// Find a way to do this with userIds instead of a username which not everyone have
-
-// func handle_admin_delete_last_command(update tgbotapi.Update, bot *tgbotapi.BotAPI) tgbotapi.MessageConfig {
-// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-// 	config := &tgbotapi.GetChatMemberConfig{
-// 		ChatConfigWithUser: tgbotapi.ChatConfigWithUser{
-// 			ChatID:             update.FromChat().ID,
-// 			SuperGroupUsername: "",
-// 			UserID:             update.SentFrom().ID,
-// 		},
-// 	}
-// 	member, err := bot.GetChatMember(*config)
-//
-// 	if err != nil {
-// 		log.Error(err)
-// 	} else {
-// 		super := member.IsAdministrator() || member.IsCreator()
-// 		if super {
-// 			// change last workout for user
-// 			username := strings.Trim(update.Message.CommandArguments(), "@")
-// 			err := rollbackLastWorkout(username)
-// 			if err != nil {
-// 				log.Error(err)
-// 			}
-// 			msg.Text = fmt.Sprintf("%s last workout cancelled", username)
-// 		}
-// 	}
-// 	return msg
-// }
