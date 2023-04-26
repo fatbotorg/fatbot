0a507a2f8ac02b731453eeade7016cf950cf39f3
Omer Hamerman <omerxx@gmail.com>
2023-04-26 23:01:30 +0100

    Admin will only work if marked in the DB

--- admin.go
+++ admin.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/charmbracelet/log"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -14,39 +13,25 @@ func isAdmin(userId int) bool {
 	return user.IsAdmin
 }
 
-func handle_admin_delete_last_command(update tgbotapi.Update, bot *tgbotapi.BotAPI) tgbotapi.MessageConfig {
+func handleAdminDeleteLastCommand(update tgbotapi.Update, bot *tgbotapi.BotAPI) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-	config := &tgbotapi.GetChatMemberConfig{
-		ChatConfigWithUser: tgbotapi.ChatConfigWithUser{
-			ChatID:             update.FromChat().ID,
-			SuperGroupUsername: "",
-			UserID:             update.SentFrom().ID,
-		},
-	}
-	member, err := bot.GetChatMember(*config)
-
-	if err != nil {
-		log.Error(err)
-	} else {
-		super := isAdmin(int(update.Message.From.ID)) && (member.IsAdministrator() || member.IsCreator())
-		if super {
-
-			users := getUsers()
-			row := []tgbotapi.InlineKeyboardButton{}
-			rows := [][]tgbotapi.InlineKeyboardButton{}
-			for _, user := range users {
-				userLabel := fmt.Sprintf("%s,%s", user.Name, user.Username)
-				row = append(row, tgbotapi.NewInlineKeyboardButtonData(userLabel, fmt.Sprint(user.TelegramUserID)))
-				if len(row) == 3 {
-					rows = append(rows, row)
-					row = row[:0]
-				}
+	super := isAdmin(int(update.Message.From.ID))
+	if super {
+		users := getUsers()
+		row := []tgbotapi.InlineKeyboardButton{}
+		rows := [][]tgbotapi.InlineKeyboardButton{}
+		for _, user := range users {
+			userLabel := fmt.Sprintf("%s,%s", user.Name, user.Username)
+			row = append(row, tgbotapi.NewInlineKeyboardButtonData(userLabel, fmt.Sprint(user.TelegramUserID)))
+			if len(row) == 3 {
+				rows = append(rows, row)
+				row = row[:0]
 			}
-			rows = append(rows, row)
-			var keyboard = tgbotapi.NewInlineKeyboardMarkup(rows...)
-			msg.ReplyMarkup = keyboard
-			msg.Text = "Pick a user"
 		}
+		rows = append(rows, row)
+		var keyboard = tgbotapi.NewInlineKeyboardMarkup(rows...)
+		msg.ReplyMarkup = keyboard
+		msg.Text = "Pick a user"
 	}
 	return msg
 }
