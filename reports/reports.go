8edb0569288bd8f129697fafdda9d21d7f5737a5
Omer Hamerman <omerxx@gmail.com>
2023-04-30 14:17:58 +0100

    reports in its FIRST PHASE

--- reports/reports.go
+++ reports/reports.go
@@ -1,6 +1,7 @@
 package reports
 
 import (
+	"fatbot/accounts"
 	"fatbot/users"
 	"fmt"
 	"os"
@@ -17,43 +18,57 @@ func CreateChart(bot *tgbotapi.BotAPI) {
 	var usersNames []string
 	var usersWorkouts []string
 	var chatId int64
-	users := users.GetUsers()
-	for _, user := range users {
-		usersNames = append(usersNames, user.GetName())
-		usersWorkouts = append(usersWorkouts, fmt.Sprint(len(user.GetWorkouts())))
-		if chatId == 0 {
-			chatId = user.ChatID
+	// var leader map[string]int
+	accounts := accounts.GetAccounts()
+	for _, account := range accounts {
+
+		// TODO: TEMP
+		if account.ChatID != -1001899294753 {
+			continue
 		}
-	}
-	usersStringSlice := "'" + strings.Join(usersNames, "', '") + "'"
-	workoutsStringSlice := strings.Join(usersWorkouts, ", ")
-	chartConfig := fmt.Sprintf(`{
+		// TODO: TEMP
+
+		users := users.GetUsers()
+		for _, user := range users {
+			if user.ChatID != account.ChatID {
+				continue
+			}
+			usersNames = append(usersNames, user.GetName())
+			usersWorkouts = append(usersWorkouts, fmt.Sprint(len(user.GetWorkouts())))
+			if chatId == 0 {
+				chatId = user.ChatID
+			}
+		}
+		usersStringSlice := "'" + strings.Join(usersNames, "', '") + "'"
+		workoutsStringSlice := strings.Join(usersWorkouts, ", ")
+		chartConfig := fmt.Sprintf(`{
 		type: 'bar',
 		data: {
 			labels: [%s],
 			datasets: [{
-			label: 'Users',
+			label: 'Workouts',
 			data: [%s]
 			}]
 		}
 	}`, usersStringSlice, workoutsStringSlice)
-	qc := quickchartgo.New()
-	qc.Config = chartConfig
-	qc.Width = 500
-	qc.Height = 300
-	qc.Version = "2.9.4"
-	qc.BackgroundColor = "white"
-	file, err := os.Create("output.png")
-	if err != nil {
-		panic(err)
-	}
+		qc := quickchartgo.New()
+		qc.Config = chartConfig
+		qc.Width = 500
+		qc.Height = 300
+		qc.Version = "2.9.4"
+		qc.BackgroundColor = "white"
+		file, err := os.Create("output.png")
+		if err != nil {
+			panic(err)
+		}
 
-	defer file.Close()
-	qc.Write(file)
+		defer file.Close()
+		qc.Write(file)
 
-	msg := tgbotapi.NewPhoto(chatId, tgbotapi.FilePath("output.png"))
-	msg.Caption = "Weekly summary"
-	if _, err = bot.Send(msg); err != nil {
-		log.Error(err)
+		msg := tgbotapi.NewPhoto(chatId, tgbotapi.FilePath("output.png"))
+		msg.Caption = "Weekly summary"
+		if _, err = bot.Send(msg); err != nil {
+			log.Error(err)
+		}
 	}
 }
