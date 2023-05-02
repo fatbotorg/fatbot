d48abda53d4665378b89f4a2fe32a4c6c282698b
Omer Hamerman <omerxx@gmail.com>
2023-05-02 17:59:48 +0100

    rejoining a team using ban/unban and a link

--- callbacks.go
+++ callbacks.go
@@ -4,11 +4,15 @@ import (
 	"fatbot/users"
 	"fmt"
 	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func handleCallbacks(update tgbotapi.Update, bot *tgbotapi.BotAPI) error {
+func handleCallbacks(fatBotUpdate FatBotUpdate) error {
+	update := fatBotUpdate.Update
+	bot := fatBotUpdate.Bot
+	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
 	switch update.CallbackQuery.Message.Text {
 	case "Pick a user to delete last workout for":
 		callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
@@ -25,22 +29,46 @@ func handleCallbacks(update tgbotapi.Update, bot *tgbotapi.BotAPI) error {
 		} else {
 			message := fmt.Sprintf("Deleted last workout for user %s\nRolledback to: %s",
 				user.GetName(), newLastWorkout.CreatedAt.Format("2006-01-02 15:04:05"))
-			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, message)
+			msg.Text = message
 			if _, err := bot.Send(msg); err != nil {
 				return err
 			}
-			user.SendPrivateMessage(bot, fmt.Sprintf("Your last workout was cancelled by the admin.\nUpdated workout: %s", newLastWorkout.CreatedAt))
+			messageToUser := tgbotapi.NewMessage(0,
+				fmt.Sprintf("Your last workout was cancelled by the admin.\nUpdated workout: %s",
+					newLastWorkout.CreatedAt))
+			user.SendPrivateMessage(bot, messageToUser)
 		}
 	case "Pick a user to rename":
 		callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
 		if _, err := bot.Request(callback); err != nil {
 			panic(err)
 		}
-		msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
 		msg.Text = fmt.Sprintf("/admin_rename %s newname", update.CallbackData())
 		if _, err := bot.Send(msg); err != nil {
 			return err
 		}
 	}
+	if strings.Contains(update.CallbackQuery.Message.Text, "rejoin his group do you approve") {
+		if update.CallbackQuery.Data == "false" {
+			msg.Text = "Declined the request"
+		} else {
+			userId, _ := strconv.ParseInt(update.CallbackData(), 10, 64)
+			user, err := users.GetUser(uint(userId))
+			if err != nil {
+				return err
+			}
+			if err := user.UnBan(fatBotUpdate.Bot); err != nil {
+				return fmt.Errorf("Issue with unbanning %s: %s", user.GetName(), err)
+			}
+			if err := user.Invite(fatBotUpdate.Bot); err != nil {
+				return fmt.Errorf("Issue with inviting %s: %s", user.GetName(), err)
+			}
+			msg.Text = "Ok, approved"
+		}
+		if _, err := bot.Send(msg); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
