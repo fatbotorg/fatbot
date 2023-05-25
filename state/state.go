d020b8f167f14b9e6a1602bb3c6de66f5b1f29e9
Omer Hamerman <omerxx@gmail.com>
2023-05-25 21:58:57 +0100

    retry and delete state

--- state/state.go
+++ state/state.go
@@ -113,7 +113,14 @@ func DeleteStateEntry(chatId int64) error {
 
 func HasState(chatId int64) bool {
 	if state, err := getState(chatId); err != nil || state == "" {
-		log.Info("No state", "id", chatId)
+		log.Info("No state", "id", chatId, "err", err)
+		time.Sleep(1 * time.Second)
+		if state, err := getState(chatId); err != nil || state == "" {
+			log.Info("No state 2", "id", chatId, "err", err)
+		}
+		if err := DeleteStateEntry(chatId); err != nil {
+			log.Error("hasstate, deletestate")
+		}
 		return false
 	}
 	return true
