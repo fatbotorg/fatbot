321a0fefd7e770b837ed43b5b7389a6f3b248144
Omer Hamerman <omerxx@gmail.com>
2023-07-01 09:27:59 +0100

    viper additions

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -1,23 +1,28 @@
 package schedule
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/go-co-op/gocron"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/spf13/viper"
 )
 
 func Init(bot *tgbotapi.BotAPI) {
-	loc, err := time.LoadLocation("Europe/Rome")
+	timezone := viper.GetString("timezone")
+	reportHour := viper.GetInt64("report.hour")
+	reportTime := fmt.Sprintf("%d:00", reportHour)
+	location, err := time.LoadLocation(timezone)
 	if err != nil {
 		log.Fatalf("Bad timezone: %s", err)
 	}
-	scheduler := gocron.NewScheduler(loc)
+	scheduler := gocron.NewScheduler(location)
 	if _, err := scheduler.Every(1).Hours().Do(func() { scanUsers(bot) }); err != nil {
 		log.Errorf("Strikes scheduler err: %s", err)
 	}
-	if _, err := scheduler.Every(1).Day().Saturday().At("18:00").Do(func() { CreateChart(bot) }); err != nil {
+	if _, err := scheduler.Every(1).Day().Saturday().At(reportTime).Do(func() { CreateChart(bot) }); err != nil {
 		log.Errorf("Reports scheduler err: %s", err)
 	}
 	scheduler.StartAsync()
