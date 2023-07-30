02000739d20a2b5d35ab5ebb43c366dc9083815e
Omer Hamerman <omerxx@gmail.com>
2023-07-30 18:42:44 +0100

    local admin utils

--- users/admin.go
+++ users/admin.go
@@ -3,12 +3,28 @@ package users
 import (
 	"fatbot/db"
 
+	"github.com/charmbracelet/log"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func SendMessageToAdmins(bot *tgbotapi.BotAPI, message tgbotapi.MessageConfig) {
-	admins := GetAdminUsers()
+func SendMessageToSuperAdmins(bot *tgbotapi.BotAPI, message tgbotapi.MessageConfig) {
+	admins := GetSuperAdminUsers()
 	for _, admin := range admins {
+		admin.SendPrivateMessage(bot, message)
+	}
+}
+
+func SendMessageToGroupAdmins(bot *tgbotapi.BotAPI, chatId int64, message tgbotapi.MessageConfig) {
+	group, err := GetGroupWithAdmins(chatId)
+	if err != nil {
+		log.Error("can't get group admins", "err", err, "group", group.Title)
+		return
+	}
+	if len(group.Admins) == 0 {
+		log.Error("no admins for goup", "group", group.Title)
+		return
+	}
+	for _, admin := range group.Admins {
 		admin.SendPrivateMessage(bot, message)
 	}
 }
@@ -23,4 +39,20 @@ func (user User) AddLocalAdmin(chatId int64) error {
 		}
 	}
 	return nil
+}
+
+func (user *User) loadManagedGroups() {
+	db := db.DBCon
+	if err := db.Preload("GroupsAdmin").Find(&user).Error; err != nil {
+		log.Error(err)
+	}
+}
+
+func (user User) IsLocalAdmin() (bool, error) {
+	db := db.DBCon
+	err := db.Preload("GroupsAdmin").Find(&user).Error
+	if err != nil {
+		return false, err
+	}
+	return len(user.GroupsAdmin) > 0, nil
 }
